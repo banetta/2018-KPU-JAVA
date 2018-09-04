4e224c7bb10747df088b914f1461c701f2ef4342
Banetta <banetion@gmail.com>
2018-09-04 13:53:47 +0900

    0830 HelloWorld.java Included

---
+++ eclipse/HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author banet
+ *
+ */
+module HelloWorld {
+}
---
+++ eclipse/HelloWorld2/src/com/HelloWorld2/main.java
@@ -0,0 +1,11 @@
+package com.HelloWorld2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!");
+
+	}
+
+}
